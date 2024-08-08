334a37bec7fdc9ed1b17b7621f7f9de806127f48
Alfredo Cano <alfredo.cano@impuls.mx>
2024-08-08 17:59:32 -0600

    Project start, created user controller and models

---
+++ testMtechBack/Models/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testMtechBack.Models
+{
+    public class UserDto
+    {
+        /*
+        DTO (Data Transfer Object) used for transferring user-related data
+        between the client and server, or within different layers of the application.
+        */
+    }
+}
---
+++ testMtechBack/Models/UserResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testMtechBack.Models
+{
+    /*
+            Represents the response structure for user-related operations.
+            This model encapsulates the data that will be returned to the client
+            in a structured format, making it easier to handle on the client side.
+    */
+    public class UserResponse
+    {
+        
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
